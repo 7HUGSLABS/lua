dbd2f5f89e32a82f5c347f426c2e392ab0f69161
Nicolas Schodet <nschodet@seven-hugs.com>
2016-03-16 15:18:54 +0100

    Allow to define LUA_ROOT from compiler command line

--- src/luaconf.h
+++ src/luaconf.h
@@ -188,7 +188,9 @@
 
 #else			/* }{ */
 
+#ifndef LUA_ROOT
 #define LUA_ROOT	"/usr/local/"
+#endif
 #define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR "/"
 #define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR "/"
 #define LUA_PATH_DEFAULT  \
